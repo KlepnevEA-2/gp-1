e526ae03e75290cb6eda84e7834bde6f823ee9d1
Klepnev <klepnev-ea@yandex.ru>
2018-06-12 14:28:14 +0300

    commit

---
+++ js/ajax.js
@@ -0,0 +1,23 @@
+$('#submit').on('click', function(e) {
+    e.preventDefault();
+    var name = $('#name').val(),
+        email = $('#email').val(),
+        phone = $('#phone').val();;
+    console.log(email);
+    $.ajax({
+        url: '/login.php',
+        method: 'POST',
+        dataType: 'json',
+        data: {
+            name: name,
+            phone: phone,
+            email: email
+        }
+    }).done(function(data) {
+        console.log(data);
+        // var html = 'email: ' + data.mail + ';name: ' + data.name + ';phone: ' + data.tel;
+        // console.log(html);
+    });
+
+});
+
--- login.php
+++ login.php
@@ -1,6 +1,18 @@
 <?php
-// хост БД
-$db_hostname = 'burgers';
-$db_database = 'users';
-$db_username = 'root';
-$db_password = '';
+
+$name = $_POST['name'];
+$phone = $_POST['phone'];
+$email = $_POST['email'];
+
+$dsn = "mysql:host=localhost;dbname=burgers;charset=utf8";
+$pdo = new PDO($dsn, 'root', '');
+$prepare = $pdo->prepare('SELECT * FROM users where mail = :uslovie1');
+//$name = $_REQUEST['mail'];
+$prepare->execute(['uslovie1' => $email]);
+if($prepare == undefined) {
+    $query = "INSERT INTO uses (name, tel, mail) VALUES ('".$_POST['name']."','".$_POST['phone']."','".$_POST['email']."')";
+    $result = mysqli_query($query) or die ("<p>ошибка запроса</p>");
+} else {
+    $data = $prepare->fetch(PDO::FETCH_OBJ);
+    echo json_encode($data);
+}
