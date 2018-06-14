818902849d1e2530f0b92a5a8ad855bc8d6c7cbc
Klepnev <klepnev-ea@yandex.ru>
2018-06-14 12:17:26 +0300

    commit

--- burgers2.sql
+++ burgers2.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Хост: 127.0.0.1:3306
--- Время создания: Июн 13 2018 г., 22:25
+-- Время создания: Июн 14 2018 г., 12:16
 -- Версия сервера: 5.6.37
 -- Версия PHP: 7.1.7
 
@@ -39,6 +39,15 @@ CREATE TABLE `orders2` (
   `comment` text COLLATE utf8_unicode_ci
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
+--
+-- Дамп данных таблицы `orders2`
+--
+
+INSERT INTO `orders2` (`id`, `user_id`, `street`, `home`, `part`, `appt`, `floor`, `comment`) VALUES
+(5, 0, 'какаято', '1', '1', '15', '189', 'Хочу еды'),
+(6, 0, 'какаято', '1', '1', '15', '189', 'ЕЩЕ ХОЧУ ЕДЫ!!!'),
+(7, 0, 'какаято', '186', '6', '4', '5', 'нужна еда');
+
 -- --------------------------------------------------------
 
 --
@@ -52,6 +61,14 @@ CREATE TABLE `users2` (
   `tel` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `count` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Дамп данных таблицы `users2`
+--
+
+INSERT INTO `users2` (`id`, `email`, `name`, `tel`, `count`) VALUES
+(4, 'klepnev-ea@yandex.ru', 'оля', '415489426', 2),
+(5, 'evgeniyanatolich@yandex.ru', 'вася', '03', 1);
 
 --
 -- Индексы сохранённых таблиц
@@ -78,12 +95,12 @@ ALTER TABLE `users2`
 -- AUTO_INCREMENT для таблицы `orders2`
 --
 ALTER TABLE `orders2`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=332;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 --
 -- AUTO_INCREMENT для таблицы `users2`
 --
 ALTER TABLE `users2`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=86;COMMIT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
